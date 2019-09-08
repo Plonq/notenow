2937d821b5f634ac5a30819b57dfee99a3ff02d4
Huon Imberger <huon.imberger@firstever.com>
2019-09-08 19:58:43 +1000

    Initial Commit

---
+++ NoteNow/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NoteNow
+//
+//  Created by Huon Imberger on 8/9/19.
+//  Copyright © 2019 Huon Imberger. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NoteNow/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NoteNow
+//
+//  Created by Huon Imberger on 8/9/19.
+//  Copyright © 2019 Huon Imberger. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
